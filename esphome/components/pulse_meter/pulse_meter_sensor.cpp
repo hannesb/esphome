a970554ceb9b169f4239f5aae5d01ee0cb73c212
hannesb <github@hannes-brunner.de>
2024-10-30 20:48:11 +0100

    Update pulse_meter_sensor.cpp

--- esphome/components/pulse_meter/pulse_meter_sensor.cpp
+++ esphome/components/pulse_meter/pulse_meter_sensor.cpp
@@ -55,25 +55,31 @@ void PulseMeterSensor::loop() {
 
   // Check if we detected a pulse this loop
   if (this->get_->count_up_ > 0 || this->get_->count_down_ > 0) {
+    bool publishState = false;
     // Keep a running total of pulses if a total sensor is configured
-    if (this->forward_sensor_ != nullptr) {
-      if (this->get_->count_up_ > 0) {
-        this->total_pulses_up_ += this->get_->count_up_;
+    if (this->get_->count_up_ > 0) {
+      this->total_pulses_up_ += this->get_->count_up_;
+      publishState = true;
+    }
+    if (this->get_->count_down_ > 0) {
+      this->total_pulses_down_ += this->get_->count_down_;
+      publishState = true;
+    }
+    if (count != 0) {
+      this->total_pulses_ += count;
+      publishState = true;
+    }
+    if (publishState) {
+      if (this->forward_sensor_ != nullptr) {
         this->forward_sensor_->publish_state(this->total_pulses_up_);
       }
-    }
-    if (this->reverse_sensor_ != nullptr) {
-      if (this->get_->count_down_ > 0) {
-        this->total_pulses_down_ += this->get_->count_down_;
+      if (this->reverse_sensor_ != nullptr) {
         this->reverse_sensor_->publish_state(this->total_pulses_down_);
       }
-    }
-    if (this->total_sensor_ != nullptr) {
-      if (count != 0) {
-        this->total_pulses_ += count;
+      if (this->total_sensor_ != nullptr) {
         this->total_sensor_->publish_state(this->total_pulses_);
       }
-    }
+    }    
 
     // We need to detect at least two edges to have a valid pulse width
     switch (this->meter_state_) {
