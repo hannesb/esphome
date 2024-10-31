847c66cebb3a745c0794d56e5d5b48acdbdf9d03
hannesb <github@hannes-brunner.de>
2024-10-31 09:45:58 +0100

    Update pulse_meter_sensor.cpp

--- esphome/components/pulse_meter/pulse_meter_sensor.cpp
+++ esphome/components/pulse_meter/pulse_meter_sensor.cpp
@@ -8,7 +8,7 @@ namespace pulse_meter {
 static const char *const TAG = "pulse_meter";
 
 void PulseMeterSensor::set_total_pulses(int32_t pulses) {
-  ESP_LOGD(TAG, "'%s': set_total_pulses %lu", this->get_name().c_str(), pulses);
+  ESP_LOGD(TAG, "'%s': set_total_pulses %ld", this->get_name().c_str(), pulses);
   this->total_pulses_ = pulses;
   if (this->total_sensor_ != nullptr) {
     this->total_sensor_->publish_state(this->total_pulses_);
